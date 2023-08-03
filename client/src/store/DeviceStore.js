c51ec10e609a2ce3b178686f4707d336d0e7b316
Oleh Noskov <oleh.noskov@nixs.com>
2023-08-03 18:27:51 +0300

    Add new some pages

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -4,12 +4,14 @@ export default class DeviceStore {
     constructor() {
         //This variable with underscore because it's agreement and this variable do not change
         this._types = [
-            {id:1, name: 'Phones'},
-            {id:2, name: 'TV'},
+            {id: 1, name: 'Phones'},
+            {id: 2, name: 'TV'},
+            {id: 3, name: 'Laptops'},
+            {id: 4, name: 'Earphones'}
         ]
         this._brands = [
-            {id:1, name: 'Samsung'},
-            {id:2, name: 'Apple'},
+            {id: 1, name: 'Samsung'},
+            {id: 2, name: 'Apple'}
         ]
         this._devices = [
             {
@@ -17,30 +19,46 @@ export default class DeviceStore {
                 name: "15 pro",
                 price: 60000,
                 rating: 0,
-                img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ffdn.gsmarena.com%2Fimgroot%2Fstatic%2Fheaders%2Fmakers%2Fapple-2023-1.jpg&tbnid=vT5TJ9akYiBbnM&vet=12ahUKEwimwuSlo8CAAxXbEhAIHebqCGcQMygBegUIARDsAQ..i&imgrefurl=https%3A%2F%2Fwww.gsmarena.com%2Fapple-phones-48.php&docid=0fQr7CAOJLv_rM&w=728&h=316&q=phones&ved=2ahUKEwimwuSlo8CAAxXbEhAIHebqCGcQMygBegUIARDsAQ",
+                img: "https://www.zdnet.com/a/img/resize/f792d73c3e241b8f40a52875616f490ea1a08e97/2023/04/24/4e586f53-afa2-452d-baf4-cc7c78c2c5fb/samsung-galaxy-a54-5g.jpg?auto=webp&fit=crop&height=1200&width=1200"
             },
             {
                 id: 2,
                 name: "13 pro max",
                 price: 60000,
                 rating: 0,
-                img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ffdn.gsmarena.com%2Fimgroot%2Fstatic%2Fheaders%2Fmakers%2Fapple-2023-1.jpg&tbnid=vT5TJ9akYiBbnM&vet=12ahUKEwimwuSlo8CAAxXbEhAIHebqCGcQMygBegUIARDsAQ..i&imgrefurl=https%3A%2F%2Fwww.gsmarena.com%2Fapple-phones-48.php&docid=0fQr7CAOJLv_rM&w=728&h=316&q=phones&ved=2ahUKEwimwuSlo8CAAxXbEhAIHebqCGcQMygBegUIARDsAQ",
+                img: "https://www.zdnet.com/a/img/resize/f792d73c3e241b8f40a52875616f490ea1a08e97/2023/04/24/4e586f53-afa2-452d-baf4-cc7c78c2c5fb/samsung-galaxy-a54-5g.jpg?auto=webp&fit=crop&height=1200&width=1200"
             },
             {
                 id: 3,
                 name: "TV 42",
                 price: 100000,
                 rating: 0,
-                img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ffdn.gsmarena.com%2Fimgroot%2Fstatic%2Fheaders%2Fmakers%2Fapple-2023-1.jpg&tbnid=vT5TJ9akYiBbnM&vet=12ahUKEwimwuSlo8CAAxXbEhAIHebqCGcQMygBegUIARDsAQ..i&imgrefurl=https%3A%2F%2Fwww.gsmarena.com%2Fapple-phones-48.php&docid=0fQr7CAOJLv_rM&w=728&h=316&q=phones&ved=2ahUKEwimwuSlo8CAAxXbEhAIHebqCGcQMygBegUIARDsAQ",
+                img: "https://www.zdnet.com/a/img/resize/f792d73c3e241b8f40a52875616f490ea1a08e97/2023/04/24/4e586f53-afa2-452d-baf4-cc7c78c2c5fb/samsung-galaxy-a54-5g.jpg?auto=webp&fit=crop&height=1200&width=1200"
             },
             {
                 id: 4,
                 name: "TV 52",
                 price: 100000,
                 rating: 0,
-                img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ffdn.gsmarena.com%2Fimgroot%2Fstatic%2Fheaders%2Fmakers%2Fapple-2023-1.jpg&tbnid=vT5TJ9akYiBbnM&vet=12ahUKEwimwuSlo8CAAxXbEhAIHebqCGcQMygBegUIARDsAQ..i&imgrefurl=https%3A%2F%2Fwww.gsmarena.com%2Fapple-phones-48.php&docid=0fQr7CAOJLv_rM&w=728&h=316&q=phones&ved=2ahUKEwimwuSlo8CAAxXbEhAIHebqCGcQMygBegUIARDsAQ",
+                img: "https://www.zdnet.com/a/img/resize/f792d73c3e241b8f40a52875616f490ea1a08e97/2023/04/24/4e586f53-afa2-452d-baf4-cc7c78c2c5fb/samsung-galaxy-a54-5g.jpg?auto=webp&fit=crop&height=1200&width=1200"
+            },
+            {
+                id: 5,
+                name: "TV 52",
+                price: 100000,
+                rating: 0,
+                img: "https://www.zdnet.com/a/img/resize/f792d73c3e241b8f40a52875616f490ea1a08e97/2023/04/24/4e586f53-afa2-452d-baf4-cc7c78c2c5fb/samsung-galaxy-a54-5g.jpg?auto=webp&fit=crop&height=1200&width=1200"
+            },
+            {
+                id: 6,
+                name: "TV 52",
+                price: 100000,
+                rating: 0,
+                img: "https://www.zdnet.com/a/img/resize/f792d73c3e241b8f40a52875616f490ea1a08e97/2023/04/24/4e586f53-afa2-452d-baf4-cc7c78c2c5fb/samsung-galaxy-a54-5g.jpg?auto=webp&fit=crop&height=1200&width=1200"
             }
         ]
+        this._selectedType = {}
+        this._selectedBrand = {}
         //Overview for this variables
         makeAutoObservable(this)
     }
@@ -57,6 +75,14 @@ export default class DeviceStore {
         this._devices = devices
     }
 
+    setSelectedType(type) {
+        this._selectedType = type
+    }
+
+    setSelectedBrand(brand) {
+        this._selectedBrand = brand
+    }
+
     get types() {
         return this._types
     }
@@ -66,6 +92,14 @@ export default class DeviceStore {
     }
 
     get devices() {
-        return this.devices
+        return this._devices
+    }
+
+    get selectedType() {
+        return this._selectedType
+    }
+
+    get selectedBrand() {
+        return this._selectedBrand
     }
 }
