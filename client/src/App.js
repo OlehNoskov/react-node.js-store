bb0075bdeb1f59b5f0f1c475be92849e950a5c13
Oleh Noskov <oleh.noskov@nixs.com>
2023-08-03 12:29:13 +0300

    Add pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+
 function App() {
     return (
         <div className="App">
-            <p>Hello Oleg!</p>
+            <BrowserRouter>
+                <AppRouter />
+            </BrowserRouter>
         </div>
     );
 }
