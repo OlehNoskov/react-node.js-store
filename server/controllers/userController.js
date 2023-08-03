b4de23798e6f61ba8a6564bc75ea2faed2a40975
Oleh Noskov <oleh.noskov@nixs.com>
2023-08-03 11:09:12 +0300

    Add mechanism for login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,8 +3,13 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {User, Basket, Type} = require('../models/models')
 
+//Generating JWT for registration and login
+const generateJWT = (id, email, role) => {
+    return jwt.sign({id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'})
+}
+
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
         const {email, password, role} = req.body
         if (!email || !password) {
             return next(ApiError.badRequest('Email or password is incorrect'))
@@ -16,16 +21,22 @@ class UserController {
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({email, role, password: hashPassword})
         const basket = await Basket.create({userId: user.id})
-        const token = jwt.sign(
-            {id: user.id, email, role},
-            process.env.SECRET_KEY,
-            {expiresIn: '24h'}
-        )
+        const token = generateJWT(user.id, user.email, user.role)
         return res.json({token})
     }
 
-    async login(req, res) {
-
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.badRequest('User hasn\'t been founded'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.badRequest('Password is incorrect!'))
+        }
+        const token = generateJWT(user.id, user.email, user.role)
+        return res.json({token})
     }
 
     async check(req, res, next) {
